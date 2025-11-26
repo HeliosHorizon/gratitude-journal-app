0af4b4b70f11da22f4b2a8649bd7ff5662569a63
HorizonMoonDive <kshitiz.j@moondive.co>
2025-11-26 15:49:02 +0530

    scheduler and server uptime removed

--- utils/dailyNotificationJob.js
+++ utils/dailyNotificationJob.js
@@ -318,7 +318,7 @@ export const startDailyJob = () => {
   global._dailyGratitudeJobStarted = true;
 
   // Runs every day at 4:05 PM server time
-  cron.schedule("32 15 * * *", async () => {
+  cron.schedule("12 16 * * *", async () => {
     try {
       console.log("⏰ Running daily gratitude notification job...");
 
